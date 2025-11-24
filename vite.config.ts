6d462adbdb024fcf09806d01c1f3e684f4dafeb3
OscarOchoaRamos01 <oscar.ochoa.r@vallegrande.edu.pe>
2025-11-24 08:36:01 -0500

    Update: arreglo de errores

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.VITE_PORT || '5000'),
-    host: process.env.VITE_HOST || 'localhost',
+    port: 5000,
+    host: '0.0.0.0',
   },
 })
